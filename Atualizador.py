874a1f9d25c0e3d623ea1671b9b938d561ac3fd2
João Urpia <joao.urpiai@gmail.com>
2026-01-13 00:02:23 -0300

    Atualização automática - 13/01/2026 00:02

--- Atualizador.py
+++ Atualizador.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import sys
 
 # --- CONFIGURAÇÕES ---
-CAMINHO_PROJETO = r"C:\Python\Dashboard_Semanal\Sauipe"
+CAMINHO_PROJETO = r"C:\Python\Dashboard_Sauipe"
 # Nota: O script agora pega TUDO, não precisa especificar o CSV
 
 def atualizar_projeto():
